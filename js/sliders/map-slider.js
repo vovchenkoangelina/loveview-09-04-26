836dc7046573f5ed3fe743862d01f66d05741ac0
qizl <qizl@MacBook-Air-qizl.local>
2026-04-16 16:41:35 +0300

    map done

--- js/sliders/map-slider.js
+++ js/sliders/map-slider.js
@@ -1,18 +1,14 @@
 export function initMapSlider() {
     const button = document.querySelector('.map__button');
-const track = document.querySelector('.map__slider-track');
-const slides = document.querySelectorAll('.map__slide');
+    const slides = document.querySelectorAll('.map__slide');
 
-let index = 0;
+    let currentIndex = 0;
 
-button.addEventListener('click', () => {
-  index++;
-
-  if (index >= slides.length) {
-    index = 0;
-  }
-
-  track.style.transform = `translateX(-${index * 100}%)`;
-  console.log("да");
-});
+    button.addEventListener('click', () => {
+        slides[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % slides.length;
+        slides[currentIndex].classList.add('active');
+    });
 }
+
+
